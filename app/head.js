19357084641aa0e8e8003d8e62842c0aba14d048
Grovillon Maison <d@grovillon.com>
2025-03-30 18:57:13 +0100

    Add head.js for App Router to detect page routing

---
+++ app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <title>Grovillon Maison</title>
+        <meta name="description" content="Silent force, lasting influence." />
+        <link rel="icon" href="/favicon.ico" />
+      </>
+    );
+  }
+  
